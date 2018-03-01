5287ed98a9d8db8da2624a3aefb5027b6960832f
Fabien <fabien.houang@epita.fr>
2018-03-01 19:23:28 +0100

    ds

---
+++ prog.py
@@ -0,0 +1,13 @@
+
+def distance(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def get_result():
+    car_pos = []
+    res = []
+    res.append()
+    for vehicule in range(0, ):
+        step = 0
+        for ride in rides:
+            if ride is not None:
+
