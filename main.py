94ba55d34ec920ab8bcd330d9de75607d26c68fa
Axel Petit <axel.petit@epita.fr>
2018-03-01 19:21:32 +0100

    Parse First line of input

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import sys
+
+file = open(sys.argv[1], "r")
+arr = file.read()
+file.close
+
+lines = arr.split("\n")
+params = lines[0].split(" ")
+
+# Parse First Line
+rows = int(params[0])
+cols = int(params[1])
+vehicules = int(params[2])
+rides = int(params[3])
+bonus = int(params[4])
+simu = int(params[5])
+
